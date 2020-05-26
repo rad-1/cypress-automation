4db5ad9d187431dcfb6b5e532429d3e517f6c3db
radicalrad1ance <jctipton123@gmail.com>
2020-05-26 16:18:30 -0400

    Init upload with google_search.spec.js test file

---
+++ cypress/integration/google_search.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Perform a Google Search without luck', () => {
+  it('Visit the Google Search page', function() {
+    cy.visit('https://www.google.com/')
+    ;}
+  );
+  it('Enter the search keyword', function() {
+    cy.get('.gLFyf').type('Kittens').should('have.value', 'Kittens')
+    ;}
+  );
+  it('Click the Google Search button', function() {
+    cy.contains('Google Search').click()
+    ;}
+  );
+  it('Verify the search title', function() {
+    cy.title().should('eq', 'Kittens - Google Search')
+  })
+});
