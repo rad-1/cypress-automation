1dd9f3aaa9ee7ceee0261164438fbfb6670df851
radicalrad1ance <jctipton123@gmail.com>
2020-05-26 16:30:56 -0400

    Refactor test and rename file

--- cypress/integration/google_search.spec.js
+++ cypress/integration/google_search.spec.js
@@ -2,18 +2,18 @@
 
 describe('Perform a Google Search without luck', () => {
   it('Visit the Google Search page', function() {
-    cy.visit('https://www.google.com/')
-    ;}
-  );
+    cy.visit('https://www.google.com/');
+  });
+
   it('Enter the search keyword', function() {
-    cy.get('.gLFyf').type('Kittens').should('have.value', 'Kittens')
-    ;}
-  );
+    cy.get('.gLFyf').type('Kittens').should('have.value', 'Kittens');
+  });
+
   it('Click the Google Search button', function() {
     cy.contains('Google Search').click()
-    ;}
-  );
+  });
+
   it('Verify the search title', function() {
-    cy.title().should('eq', 'Kittens - Google Search')
+    cy.title().should('eq', 'Kittens - Google Search');
   })
-});
+})
